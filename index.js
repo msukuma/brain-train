bdc8a8f38ccd24aa7e999b48736d1cb1bf4cb739
Nixon Lolila Ramin <nixonlr@gmail.com>
2017-09-03 15:39:29 +0300

    almost mvp

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import MentalMath from './lib/mental-math';
+import MentalMath from './lib/mental-math/mental-math';
 
 const g =  <MentalMath config={{}}/>;
+
 ReactDom.render(
   g,
   document.getElementById('test')
