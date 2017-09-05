1eb889afda55dd9120ea94e49004ae4eabed7a86
Nixon Lolila Ramin <nixonlr@gmail.com>
2017-09-06 00:16:29 +0300

    unsed tests removed

--- test/lib/number_.js
+++ test/lib/number_.js
@@ -1,9 +1,1 @@
-import Number_ from '../../lib/number_';
-import typeOf from '../../lib/type-of';
-import strings from '../../lib/constants';
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
 
-describe('#Number_', () => {
-});
--- test/lib/operation.js
+++
@@ -1,8 +0,0 @@
-import Operation from '../../lib/operation';
-import Number_ from '../../lib/number_';
-import ArgumentError from '../../lib/errors';
-import typeOf from '../../lib/type-of';
-import { expect } from 'chai';
-
-describe('#Operation', () => {
-});
